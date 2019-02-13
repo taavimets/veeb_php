024c5772ef2c54531f38137d414fd1016334ea6a
taavi.mets <taavi.mets@khk.ee>
2019-02-13 14:22:09 +0200

    variables ülesseadmine

---
+++ php_alused/muutujad/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taavi.mets
+ * Date: 13.02.2019
+ * Time: 14:14
+ */
+$eesnimi = 'Taavi';
+$perenimi = 'Mets';
+$vanus = '18';
+$kaal = '56';
+
+//var_dump($eesnimi); // muutuja sisu test vaatamine
