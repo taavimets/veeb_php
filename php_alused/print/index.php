4c95be7d0df5c84cba47cdec6324af4d599b0c50
taavi.mets <taavi.mets@khk.ee>
2019-02-13 13:14:39 +0200

    Valgusfoori tegemine

--- php_alused/print/index.php
+++ php_alused/print/index.php
@@ -1,18 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: taavi.mets
- * Date: 13.02.2019
- * Time: 12:14
- */
-echo '<span style="font-size: 35px">----<br></span>';
-echo '<span style="font-size: 40px">|</span>';
-echo '<span style="color:red; font-size: 40px">O</span>';
-echo '<span style="font-size: 40px">|<br></span>';
-echo '<span style="font-size: 40px">|</span>';
-echo '<span style="color:yellow; font-size: 40px">O</span>';
-echo '<span style="font-size: 40px">|<br></span>';
-echo '<span style="font-size: 40px">|</span>';
-echo '<span style="color:green; font-size: 40px">O</span>';
-echo '<span style="font-size: 40px">|<br></span>';
-echo '<span style="font-size: 35px">----<br></span>';
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="stiil.css">
+</head>
+<body>
+<div class="main">
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: joel.purasson
+     * Date: 13.02.2019
+     * Time: 12:14
+     */
+    echo '<div id="punane"></div>';
+    echo '<div id="kollane"></div>';
+    echo '<div id="roheline"></div>';
+    ?>
+</div>
+</body>
+</html>
