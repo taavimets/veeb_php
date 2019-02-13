0c02092106ac143252d2671fa13b1d608c45246c
taavi.mets <taavi.mets@khk.ee>
2019-02-13 15:18:36 +0200

    suvalise arvu näitamine kastis

---
+++ php_alused/loops/index.php
@@ -0,0 +1,30 @@
+<style>
+    div {
+        margin: auto;
+        width: 100px;
+        height: 100px;
+        font-size: 80px;
+        vertical-align: middle;
+        text-align: center;
+    }
+    .paaris{
+        background: red;
+    }
+    .paaritu{
+        background: green;
+    }
+</style>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taavi.mets
+ * Date: 13.02.2019
+ * Time: 14:14
+ */
+$arv = rand(0, 100);
+$jaak = $arv % 2;
+if($jaak == 0) {
+    echo '<div class="paaris">'.$arv.'</div>';
+} else {
+    echo '<div class="paaritu">'.$arv.'</div>';
+}
