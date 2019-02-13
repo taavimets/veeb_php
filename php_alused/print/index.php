1d0c9ae16b850e252fa3d9b82906dbc3f151bfe4
taavi.mets <taavi.mets@khk.ee>
2019-02-13 13:10:33 +0200

    Valgusfoori tegemine

--- php_alused/print/index.php
+++ php_alused/print/index.php
@@ -5,12 +5,14 @@
  * Date: 13.02.2019
  * Time: 12:14
  */
-echo "Hello wurld<br>";
-echo "Hello wurld<br>";
-echo "Hello wurld<br>";
-
-echo '<hr>';
-
-print "Hello wurld<br>";
-print "Hello wurld<br>";
-print "Hello wurld<br>";
+echo '<span style="font-size: 35px">----<br></span>';
+echo '<span style="font-size: 40px">|</span>';
+echo '<span style="color:red; font-size: 40px">O</span>';
+echo '<span style="font-size: 40px">|<br></span>';
+echo '<span style="font-size: 40px">|</span>';
+echo '<span style="color:yellow; font-size: 40px">O</span>';
+echo '<span style="font-size: 40px">|<br></span>';
+echo '<span style="font-size: 40px">|</span>';
+echo '<span style="color:green; font-size: 40px">O</span>';
+echo '<span style="font-size: 40px">|<br></span>';
+echo '<span style="font-size: 35px">----<br></span>';
