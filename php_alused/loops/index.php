4c2fb391e198493ecfcfd6d6ccd49bdbe64c8880
taavi.mets <taavi.mets@khk.ee>
2019-02-19 09:59:48 +0200

    tabeli tegemine arvude jaoks

--- php_alused/loops/index.php
+++ php_alused/loops/index.php
@@ -1,28 +1,73 @@
-<?php
-header('Refresh: 2');
-?>
-<style>
-    div {
-        margin: auto;
-        width: 100px;
-        height: 100px;
-        font-size: 80px;
-        vertical-align: middle;
-        text-align: center;
-    }
-    .paaris{
-        background: red;
-    }
-    .paaritu{
-        background: green;
-    }
-    .algarv{
-        background: deepskyblue;
-    }
-    .tavaline{
-        background: lightgray;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sookla menuu</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossorigin="anonymous">
+</head>
+<body>
+<div class="container text-center">
+    <!-- tabel -->
+    <table class="table">
+        <!-- tabeli header -->
+        <thead>
+        <tr>
+            <th>arv</th>
+            <th>paaris</th>
+            <th>paaritu</th>
+            <th>algarv</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        for($arv = 0; $arv <= 15; $arv++){
+            if($arv % 2 == 0) {
+                $kasPaaris = true;
+            } else {
+                $kasPaaris = false;
+            }
+            $jagaja = 2;
+            if($arv == 0 or $arv == 1) {
+                $kasAlgarv = true;
+            } else {
+                while ($arv % $jagaja != 0) $jagaja++;
+                if ($arv == $jagaja) {
+                    $kasAlgarv = true;
+                } else {
+                    $kasAlgarv = false;
+                }
+            }
+            echo '
+                    <tr>
+                        <td>'.$arv.'</td>
+                        <td class="paaris">';
+            if($kasPaaris) {
+                echo '<i class="fas fa-angle-down"></i>';
+            }
+            echo '</td>';
+            echo '<td class="paaritu">';
+            if(!$kasPaaris) {
+                echo '<i class="fas fa-angle-down"></i>';
+            }
+            echo '</td>';
+            echo '<td class="algarv">';
+            if($kasAlgarv) {
+                echo '<i class="fas fa-angle-down"></i>';
+            }
+            echo '</td>';
+            echo '</tr>';
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
 <?php
 /**
  * Created by PhpStorm.
@@ -30,17 +75,3 @@ header('Refresh: 2');
  * Date: 13.02.2019
  * Time: 14:14
  */
-    $arv = rand(0, 100);
-    $jagaja = 2;
-    while($arv % $jagaja != 0){
-        $jagaja++;
-    }
-    if($arv == $jagaja) {
-        echo '<div class="algarv">'.$arv.'</div>';
-    } else {
-        if ($arv % 2 == 0) {
-            echo '<div class="paaris">' . $arv . '</div>';
-        } else {
-            echo '<div class="paaritu">' . $arv . '</div>';
-        }
-    }
