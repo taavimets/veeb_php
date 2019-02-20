07cbddd21116ac2119004d960cea024826a21781
taavi.mets <taavi.mets@khk.ee>
2019-02-20 15:06:45 +0200

    array kasutamine

---
+++ php_alused/array/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taavi.mets
+ * Date: 20.02.2019
+ * Time: 14:58
+ */
+    $peppaPig = array(
+        'Peppa',
+        'naine',
+        4,
+        1.04
+    );
+    var_dump($peppaPig);
+
+    echo '<pre>';
+    print_r($peppaPig);
+    echo '</pre>';
