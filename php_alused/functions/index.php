bbcbefc8820f7d557fddd9e2f2ff9e0c43f8c6d3
taavi.mets <taavi.mets@khk.ee>
2019-02-20 14:22:16 +0200

    mingi koodi parandus, aga tabelit ei näita

--- php_alused/functions/index.php
+++ php_alused/functions/index.php
@@ -6,73 +6,7 @@
  * Time: 12:08
  */
 
-function kasPaaris($arv){
-    if($arv % 2 == 0){
-        $kontroll = true;
-    } else {
-        $kontroll = false;
-    }
-    return $kontroll;
-}
-
-function kasAlgarv($arv){
-    $jagaja = 2;
-    if($arv == 0 or $arv == 1) {
-        $kontroll = true;
-    } else {
-        while ($arv % $jagaja != 0) $jagaja++;
-        if ($arv == $jagaja) {
-            $kontroll = true;
-        } else {
-            $kontroll = false;
-        }
-    }
-    return $kontroll;
-}
-
-function tabeliPais(){
-    echo '
-        <thead>
-                <tr>
-                    <th>arv</th>
-                    <th>paaris</th>
-                    <th>paaritu</th>
-                    <th>algarv</th>
-                </tr>
-        </thead>
-        ';
-}
-
-    function tabeliRida($arv){
-        echo '<tr>';
-            echo '<td>'.$arv.'</td>';
-            echo '<td class="paaris">';
-            if(kasPaaris($arv)){
-                echo '<i class="fas fa-angle-down"></i>';
-            }
-        echo '<tr>';
-        echo '<td>'.$arv.'</td>';
-        echo '<td class="paaritu">';
-        if(kasPaaris($arv)){
-            echo '<i class="fas fa-angle-down"></i>';
-        }
-        echo '<tr>';
-        echo '<td>'.$arv.'</td>';
-        echo '<td class="algarv">';
-        if(kasPaaris($arv)){
-            echo '<i class="fas fa-angle-down"></i>';
-        }
-        echo '</td>';
-     echo '</tr>';
-}
-
-function tabel($ridadeArv){
-        echo '<table>';
-        echo tabeliPais();
-        echo '<tbody>';
-        for($reanumber = 0; $reanumber <= $ridadeArv; $reanumber++){
-            tabeliRida($reanumber);
-        }
-        echo '</tbody>';
-        echo '</table>';
-}
+require_once 'fnc.php';
+lehePais();
+tabel(15);
+?>
