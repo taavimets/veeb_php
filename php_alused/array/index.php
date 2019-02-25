5655c3c774c9b8a1ccd9d1315776db78bc7dfb81
taavi.mets <taavi.mets@khk.ee>
2019-02-25 08:56:13 +0200

    masiivide tegemine $-iga

--- php_alused/array/index.php
+++ php_alused/array/index.php
@@ -5,43 +5,18 @@
  * Date: 20.02.2019
  * Time: 14:58
  */
-$porsad = array(
-    array(
-        'George',
-        'mees',
-        2,
-        0.95
-    ),
-    array(
-        'Peppa',
-        'naine',
-        4,
-        1.04
-    )
+$george = array(
+    'nimi' => 'George',
+    'sugu' => 'mees',
+    'vanus' => 2,
+    'pikkus' => 0.95
+);
+$peppa = array(
+    'nimi' => 'Peppa',
+    'sugu' => 'naine',
+    'vanus' => 4,
+    'pikkus' => 1.04
 );
 
-$porsad[0][] = 'sinine';
-$porsad[1][] = 'punane';
-
-echo $porsad[0][0].'<br>';
-echo $porsad[0][1].'<br>';
-echo $porsad[0][2].'<br>';
-echo $porsad[0][3].'<br>';
-
-echo '<hr>';
-
-for($i = 0; $i < count($porsad); $i++){
-    for ($j = 0; $j < count($porsad[$i]); $j++){
-        echo $porsad[$i][$j].'<br>';
-    }
-    echo '<hr>';
-}
-
-echo '<hr>';
-
-foreach ($porsad as $porsas){
-    foreach ($porsas as $element){
-        echo $element.'<br>';
-    }
-    echo '<hr>';
-}
+echo $george['nimi'].' on '.$george['vanus'].' aastat vana<br>';
+echo $peppa['nimi'].' on '.$peppa['vanus'].' aastat vana<br>';
