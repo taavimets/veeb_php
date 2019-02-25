935aee9cc83da3f0996a3332cd7d093a56b23cf5
taavi.mets <taavi.mets@khk.ee>
2019-02-25 13:13:33 +0200

    silindri, koonuse ja kera ruumalade arvutamine

--- php_alused/forms/vormi_tootlus.php
+++ php_alused/forms/vormi_tootlus.php
@@ -5,12 +5,17 @@
  * Date: 25.02.2019
  * Time: 11:59
  */
+extract ($_GET);
 foreach ($_GET as $nimetus => $vaartus){
-    if(strlen($_GET[$nimetus]) == 0){
-        header('Location: index.html');
-        exit;
-    }
+    echo $nimetus.'=>'.$vaartus.'<br>';
 }
-foreach ($_GET as $nimetus => $vaartus){
-    echo $nimetus.' => '.$vaartus.'<br>';
-}
+$silRuumala = (3.14 * $raadius * $raadius * $korgus);
+echo 'Silindri ruumala on '.$silRuumala.'<br>';
+
+$kooRuumala = (3.14 * $raadius * $raadius * $korgus * 0.3);
+echo 'Koonuse ruumala on '.$kooRuumala.'<br>';
+
+$keraRuumala = (1.3 * 3.14 * $raadius * $raadius * $korgus);
+echo 'Kera ruumala on '.$keraRuumala.'<br>';
+
+
