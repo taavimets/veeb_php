0610bf544d7ac1a334b2971aa8d9e2749d570b1c
taavi.mets <taavi.mets@khk.ee>
2019-02-25 12:17:28 +0200

    andmete esitamine vormiga ja vormi lehekülg

---
+++ php_alused/forms/vormi_tootlus.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taavi.mets
+ * Date: 25.02.2019
+ * Time: 11:59
+ */
+echo '<pre>';
+print_r($_GET);
+echo '</pre>';
+echo '<hr>';
+
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+echo '<hr>';
+
+echo '<pre>';
+print_r($_REQUEST);
+echo '</pre>';
+echo '<hr>';
