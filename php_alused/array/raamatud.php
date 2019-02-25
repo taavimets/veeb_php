a2ba6a170fd33b1ac217d2e11b78fc7cc49ff2d4
taavi.mets <taavi.mets@khk.ee>
2019-02-25 10:46:46 +0200

    loodud funktsiooni tabeli loomiseks

--- php_alused/array/raamatud.php
+++ php_alused/array/raamatud.php
@@ -5,25 +5,26 @@
  * Date: 25.02.2019
  * Time: 9:25
  */
+$raamatud = array(
 $raamat1 = array(
     'nimi' => 'Arvutitehnika Riistvara',
     'hind' => 31.49,
     'autor' => 'Teet Evartson',
     'lehekulgi' => 360
-);
+),
 $raamat2 = array(
     'nimi' => 'Noor Leegion',
     'hind' => 17.95,
     'autor' => 'Hanno Ojalo',
     'lehekulgi' => 120
-);
+),
 $raamat3 = array(
     'nimi' => 'Minu Dublin',
     'hind' => 5.99,
     'autor' => 'Kristiina Piip',
     'lehekulgi' => 240
+),
 );
-
 echo '<pre>';
 print_r($raamat1);
 echo '</pre>';
@@ -35,33 +36,18 @@ echo '</pre>';
 echo '<pre>';
 print_r($raamat3);
 echo '</pre>';
-?>
 
-<table style="width: 50%;" border="3">
-<tbody>
-<tr>
-<td>Raamatu nimi</td>
-<td>Hind</td>
-<td>Autor</td>
-<td>Lehekulgi</td>
-</tr>
-<tr>
-<td>Arvutitehnika Riistvara</td>
-<td>31.49</td>
-<td>Teet Evartson</td>
-<td>360</td>
-</tr>
-<tr>
-<td>Noor Leegion</td>
-<td>17.95</td>
-<td>Hanno Ojalo</td>
-<td>120</td>
-</tr>
-<tr>
-<td>Minu Dublin</td>
-<td>5.99</td>
-<td>Kristiina Piip</td>
-<td>240</td>
-</tr>
-</tbody>
-</table>
+function tabeliPais($andmed){
+    echo '<thead>';
+        echo '<tr>';
+        foreach ($andmed as $element){
+            echo '<th>'.$element.'</th>';
+        }
+        echo '</tr>';
+    echo '</thead>';
+}
+
+$tabeli_pealkiri = array_keys($raamatud[0]);
+echo '<table border="1">';
+tabeliPais($tabeli_pealkiri);
+echo '</table>';
