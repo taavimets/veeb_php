b86f4e5dc163da8877a0432d69f035d359b66df7
taavi.mets <taavi.mets@khk.ee>
2019-03-07 09:58:16 +0200

    index.php faili teksti vähendamine ja queryid panna uute faili

---
+++ php_alused/db/db_fnc.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taavi.mets
+ * Date: 07.03.2019
+ * Time: 9:46
+ */
+function dbConnect($h, $u, $p, $db){
+    $connect = mysqli_connect($h, $u, $p, $db);
+    if($connect == false){
+        echo 'Probleem andmebaasi ühendamisega<br>';
+        exit;
+    }
+    return $connect;
+}
+
+function query($conn, $sql){
+    $result = mysqli_query($conn, $sql);
+    if($result == false){
+        echo 'Probleem päringuga: <b>'.$sql.'</b><br>';
+        echo mysqli_error($conn).'<br>';
+        echo mysqli_errno($conn).'<br>';
+    }
+    return $result;
+}
+
+function dataQuery($conn, $sql){
+    $result = query($conn, $sql);
+    if($result != false){
+        $data = array();
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $data[] = $row;
+        }
+    }
+    if(count($data) == 0){
+        return false;
+    }
+    return $data;
+}
