012cf21d550831770579bdb16055946df610babb
taavi.mets <taavi.mets@khk.ee>
2019-03-27 12:26:32 +0200

    menüü loomine andmebaasi abiga

---
+++ OOP_PHP/oop_php_app-master/controllers/login_do.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taavi.mets
+ * Date: 12.03.2019
+ * Time: 10:00
+ */
--- OOP_PHP/oop_php_app-master/index.php
+++ OOP_PHP/oop_php_app-master/index.php
@@ -13,7 +13,32 @@ $main->set('js', $js->parse());
 $main->set('lang', $http->get('lang_id'));
 $main->set('title', 'App Example Title');
 $mainContent = new Template('main_content');
-$mainContent->set('header', 'Page Header');
+
+
+$sql = 'SELECT * FROM dish_types';
+$dishTypes = $db->getData($sql);
+foreach ($dishTypes as $dishTypeData){
+    $dishType= new Template('type');
+    $dishTypeName = new Template('type_name');
+    $dishTypeName->set('type_name', $dishTypeData['type_name']);
+    $dishTypeName->set('type_icon', $dishTypeData['type_icon']);
+    $dishType->set('type_name', $dishTypeName->parse());
+
+    $dishData = new Template('type_data');
+    $dishData->set('type_name', $dishTypeData['type_name']);
+    $dish = new Template('dish');
+    $sql = 'SELECT * FROM dishes WHERE type_id='.fixDb($dishTypeData['type_id']);
+    $dishes = $db->getData($sql);
+    foreach ($dishes as $dishContent){
+        $dish->set('dish_name', $dishContent['dish_name']);
+        $dish->set('dish_description', $dishContent['dish_description']);
+        $dish->set('dish_price', $dishContent['dish_price']);
+    }
+    $dishData->set('dishes', $dish->parse());
+    $dishType->set('type_data', $dishData->parse());
+
+    $mainContent->add('menu', $dishType->parse());
+}
 require_once 'nav.php'; // nav element
 // page content from controller
 // add action control
